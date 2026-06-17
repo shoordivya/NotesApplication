da82bd5ab1e5c177d344ff8b5f80687595491e25
Divya Shoor <shoordivya22@gmail.com>
2026-06-17 23:49:01 +0530

    First Commit

---
+++ notes-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './components/Hero.jsx'
+const App = () => {
+  return (
+    <div className='bg-black min-h-screen w-full '>
+      <Hero />
+    </div>
+  )
+}
+
+export default App
+
---
+++ notes-frontend/src/components/Hero.jsx
@@ -0,0 +1,156 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="w-full flex lg:flex-row flex-col items-center justify-center gap-5">
+      <div className="h-screen lg:w-1/2 flex flex-col items-center justify-center gap-5">
+        <form>
+          <input
+            className="border-amber-200 text-blue-300"
+            placeholder="Enter Title"
+          />
+          <textarea
+            className="border-amber-200 text-blue-300"
+            placeholder="Enter Description"
+          />
+        </form>
+      </div>
+      <div className="h-screen grid overflow-scroll grid-cols-1 sm:items-center sm:justify-items-center sm:grid-cols-3 lg:grid-cols-4 bg-amber-400 w-full lg:w-1/2 items-start gap-5">
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Welcome to Notes App
+          </h3>
+          <p className="text-md text-blue-900">
+            Create and manage your notes with ease.
+          </p>
+        </div>
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Hi my name is divya
+          </h3>
+          <p className="text-md text-blue-900">
+            I ma recent graduted with Masters in computer Applications.
+          </p>
+        </div>
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Hi my name is divya
+          </h3>
+          <p className="text-md text-blue-900">
+            I ma recent graduted with Masters in computer Applications.
+          </p>
+        </div>
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Hi my name is divya
+          </h3>
+          <p className="text-md text-blue-900">
+            I ma recent graduted with Masters in computer Applications.
+          </p>
+        </div>
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Hi my name is divya
+          </h3>
+          <p className="text-md text-blue-900">
+            I ma recent graduted with Masters in computer Applications.
+          </p>
+        </div>
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Hi my name is divya
+          </h3>
+          <p className="text-md text-blue-900">
+            I ma recent graduted with Masters in computer Applications.
+          </p>
+        </div>
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Hi my name is divya
+          </h3>
+          <p className="text-md text-blue-900">
+            I ma recent graduted with Masters in computer Applications.
+          </p>
+        </div>
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Hi my name is divya
+          </h3>
+          <p className="text-md text-blue-900">
+            I ma recent graduted with Masters in computer Applications.
+          </p>
+        </div>
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Hi my name is divya
+          </h3>
+          <p className="text-md text-blue-900">
+            I ma recent graduted with Masters in computer Applications.
+          </p>
+        </div>
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Hi my name is divya
+          </h3>
+          <p className="text-md text-blue-900">
+            I ma recent graduted with Masters in computer Applications.
+          </p>
+        </div>
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Hi my name is divya
+          </h3>
+          <p className="text-md text-blue-900">
+            I ma recent graduted with Masters in computer Applications.
+          </p>
+        </div>
+        <div
+          id="card"
+          className="h-40 w-40 rounded-2xl p-5 m-2 overflow-scroll items-center justify-center gap-5 bg-blue-300"
+        >
+          <h3 className="text-lg leading-tight font-bold text-blue-700">
+            Hi my name is divya
+          </h3>
+          <p className="text-md text-blue-900">
+            I ma recent graduted with Masters in computer Applications.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
